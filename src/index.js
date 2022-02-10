01c00f739c09378cee805dbfe0dce47ec38839fb
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-10 17:04:34 +0300

    Плавный скролл 4
    Плавный скролл 4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import phoneList from './modules/phoneList'
 import headerMenu from './modules/headerMenu'
+import scroll from './modules/scroll'
 
 phoneList()
 headerMenu()
+scroll()
---
+++ src/modules/scroll.js
@@ -0,0 +1,21 @@
+const scroll = () => {
+  const smoothLinks = document.querySelectorAll('a[href^="#main"],a[href^="#formula"],a[href^="#repair-types"],a[href^="#portfolio"],a[href^="#transparency"],a[href^="#director"],a[href^="#reviews"],a[href^="#faq"],a[href^="#main"]');
+  for (let smoothLink of smoothLinks) {
+      smoothLink.addEventListener('click', function (e) {
+          e.preventDefault();
+          const id = smoothLink.getAttribute('href');
+  
+          document.querySelector(id).scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+          });
+          let menu = document.querySelector('.popup-menu')
+          let menuBody = document.querySelector('.popup-dialog-menu')
+          if (menu.style.visibility = "visible") {
+            menu.style.visibility = "hidden"
+            menuBody.style.transform = "translate3d(845px,0,0)"
+        }
+      });
+  };
+}
+export default scroll
