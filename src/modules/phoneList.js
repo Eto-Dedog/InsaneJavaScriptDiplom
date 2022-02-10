6c1a8b67f5624674418539f9d0bf0f84304fa794
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-10 14:46:28 +0300

    1 Меню телефона
    Меню смены телефона

--- src/modules/phoneList.js
+++ src/modules/phoneList.js
@@ -9,13 +9,13 @@ const phoneList = () => {
       phone.style.opacity = "1"
       phone.style.marginTop = "25px"
       arrow.style.transform = "rotate(180deg)"
-      arrow.style.transition = "all 0.5s"
+      arrow.style.transition = "all 0.6s"
     } else if (phone.style.opacity = '1') {
         console.log('2');
         phone.style.opacity = "0"
         phone.style.marginTop = "0px"
         arrow.style.transform = "rotate(0deg)"
-        arrow.style.transition = "all 0.5s"
+        arrow.style.transition = "all 0.6s"
     }
   });
 }
