47670288ae16b0c9880569c496d4f91c73e3ea04
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-10 13:24:52 +0300

    Сделал список телефонов
    Сделал список телефонов

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import test from './modules/test';
+import phoneList from './modules/phoneList';
 
-test();
+phoneList();
---
+++ src/modules/phoneList.js
@@ -0,0 +1,22 @@
+const phoneList = () => {
+  let listBtn = document.querySelector('.header-contacts__arrow')
+  let phone = document.querySelector('.header-contacts__phone-number-accord .header-contacts__phone-number')
+  let arrow = document.querySelector('.header-contacts__arrow img')
+  phone.style.opacity = "0"
+  listBtn.addEventListener("click", () => {
+    if (phone.style.opacity === '0') {
+      console.log('1');
+      phone.style.opacity = "1"
+      phone.style.marginTop = "25px"
+      arrow.style.transform = "rotate(180deg)"
+      arrow.style.transition = "all 0.5s"
+    } else if (phone.style.opacity = '1') {
+        console.log('2');
+        phone.style.opacity = "0"
+        phone.style.marginTop = "0px"
+        arrow.style.transform = "rotate(0deg)"
+        arrow.style.transition = "all 0.5s"
+    } 
+  });
+}
+export default phoneList
