e1c834a86a7f37123f99b3f27892e3dad84f5577
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-11 19:50:57 +0300

    6 Во всех полях, где необходимо ввести номер телефона, нужно реализовать маску
    Во всех полях, где необходимо ввести номер телефона, нужно реализовать маску и запрет ввода любых символов, кроме цифр

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import phoneList from './modules/phoneList'
 import headerMenu from './modules/headerMenu'
 import scroll from './modules/scroll'
 import openAllServices from './modules/openAllServices'
+import phoneMask from './modules/phoneMask'
 
 phoneList()
 headerMenu()
 scroll()
 openAllServices()
+phoneMask()
