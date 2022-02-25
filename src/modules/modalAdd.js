b0a2d358e09f0a025aaf2683d6a593adf1092702
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2022-02-25 12:28:25 +0300

    (2) 8 Модальное окно ""Добавление услуг'
    (2) 8 Модальное окно ""Добавление услуг'

--- src/modules/modalAdd.js
+++ src/modules/modalAdd.js
@@ -1,6 +1,8 @@
 const modalAdd = () => {
   let openModalAdd = document.querySelector('.btn-addItem')
   let closeModalAdd = document.querySelector('.button__close')
+  let cancelModalAdd = document.querySelector('.cancel-button')
+  let sendModalAdd = document.querySelector('.button-ui_firm')
   let modalAdd = document.querySelector('#modal')
 
   openModalAdd.addEventListener('click', function (e) {
@@ -8,6 +10,14 @@ const modalAdd = () => {
     modalAdd.style.display = "flex"
   })
   closeModalAdd.addEventListener('click', function (e) {
+    e.preventDefault()
+    modalAdd.style.display = "none"
+  })
+  cancelModalAdd.addEventListener('click', function (e) {
+    e.preventDefault()
+    modalAdd.style.display = "none"
+  })
+  sendModalAdd.addEventListener('click', function (e) {
     e.preventDefault()
     modalAdd.style.display = "none"
   })
